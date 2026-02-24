b8d42cbeae7c4ce13b98427c2f3adbcd27d1f131
Tanmay-Ghule <tanmayghule359@gmail.com>
2026-02-24 11:20:25 +0530

    Fix firebase admin config

--- backend/firebaseAdmin.js
+++ backend/firebaseAdmin.js
@@ -1,12 +1,13 @@
 import admin from "firebase-admin";
-import fs from "fs";
 
-const serviceAccount = JSON.parse(
-  fs.readFileSync("./serviceAccountKey.json", "utf8")
-);
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    }),
+  });
+}
 
 export default admin;
