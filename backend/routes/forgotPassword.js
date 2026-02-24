fd55e63501a4e798e87533c7f22309b9e7ea161a
Tanmay Ghule <153490672+Tanmay-ghule@users.noreply.github.com>
2026-02-24 13:19:06 +0530

    Update forgotPassword.js

--- backend/routes/forgotPassword.js
+++ backend/routes/forgotPassword.js
@@ -18,9 +18,7 @@ const twilioClient = twilio(
 
 /* EMAIL TRANSPORTER */
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
