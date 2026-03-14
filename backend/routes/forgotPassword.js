1fa00045c6c4935c62f9fd4ebc4c390e56060deb
Tanmay-Ghule <tanmayghule359@gmail.com>
2026-03-15 01:26:30 +0530

    Temp forgotPasswordChange2

--- backend/routes/forgotPassword.js
+++ backend/routes/forgotPassword.js
@@ -41,10 +41,10 @@ router.post("/request", async (req, res) => {
 
     if (!user) return res.status(404).json({ error: "User not found" });
 
+    const oneDayAgo = 24 * 60 * 60 * 1000;
     if (
       user.lastPasswordResetRequest &&
-      Date.now() - new Date(user.lastPasswordResetRequest).getTime() 
-        24 * 60 * 60 * 1000
+      Date.now() - new Date(user.lastPasswordResetRequest).getTime() < oneDayAgo
     ) {
       return res.status(429).json({
         error: "You can use this option only once per day.",
